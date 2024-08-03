4abf701dc0102a09dc5c7ce829c1e6a77525235f
binayak20 <csebinayak.ku@gmail.com>
2024-08-03 20:38:32 +0600

    1. initial commit 2. layout design 3. db setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import Header from "./_components/Header";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,8 +18,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className="light">
+        <body className={inter.className}>
+          <Header />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
